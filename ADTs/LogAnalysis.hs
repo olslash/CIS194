51d7549311ebf2ecdbe4111c4e899d48c0e9fa39
Mitch R <olordhux+github@gmail.com>
2015-04-12 01:06:28 -0700

    binary tree insert

--- ADTs/LogAnalysis.hs
+++ ADTs/LogAnalysis.hs
@@ -4,13 +4,15 @@ module ADTs.LogAnalysis where
 import Log
 
 parseMessage :: String -> LogMessage
-parseMessage s = parse $ words s
-    where parse (w:ws) = case w of "I" -> parseInfo ws
-                                   "W" -> parseWarning ws
-                                   "E" -> parseError ws
-                                   _  -> Unknown "Unknown prefix"
-          parse [] = Unknown "Empty log message"
+parseMessage s = parse' $ words s
+    where parse' (w:ws) = case w of "I" -> parseInfo ws
+                                    "W" -> parseWarning ws
+                                    "E" -> parseError ws
+                                    _  -> Unknown "Unknown prefix"
+          parse' [] = Unknown "Empty log message"
 
+parse :: String -> [LogMessage]
+parse input = map parseMessage (lines input)
 
 parseInfo :: [String] -> LogMessage
 parseInfo (time:msg) = LogMessage Info (read time :: TimeStamp) (unwords msg)
@@ -25,4 +27,14 @@ parseError (code:time:msg) = LogMessage (Error (read code :: Int))
                                         (read time :: TimeStamp) 
                                         (unwords msg)
 parseError _ = Unknown "Badly-formed error message"
+
+-------------
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert msg@(LogMessage _ newTime _) tree@(Node l logmsg@(LogMessage _ nodeTime _) r)
+    | newTime < nodeTime = Node (insert msg l) logmsg r
+    | newTime > nodeTime = Node l logmsg (insert msg r)
+    | otherwise          = tree
+insert msg@(LogMessage _ _ _) Leaf = Node Leaf msg Leaf
+insert _ tree = tree
 
